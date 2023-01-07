755e9061c395bdf619b886ab444770d2488b67ac
Aditya_Devalalikar <adityadevalalikar@gmail.com>
2023-01-07 11:46:52 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <nav className="bg-slate-100">
         <div>
-          <h1>Aditya Sudhir Devalalikar</h1>
+          <h1 className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Aditya Sudhir Devalalikar</h1>
         </div>
         <ul className="fontsize">
           <li>Home</li>
@@ -15,7 +15,13 @@ function App() {
         </ul>
       </nav>
       <div className="content-flex bg-slate-200">
-        <h1> Home </h1>
+        <div class="flex flex-wrap justify-center">
+          <img
+            src="https://github.com/aditya-devalalikar/react.js-app/blob/main/src/assets/ad_img.png"
+            className="p-1 bg-white border rounded max-w-sm"
+            alt="..."
+          />
+        </div>
       </div>
       <div className="content-flex bg-slate-300">
         <h1> About </h1>
